e45350d0e8c5f0e0caf409233bf7e10a9a50f717
ElizabethBrooks <merimaat1@gmail.com>
2016-04-28 17:34:55 -0400

    Updated comments and error message

--- MiniShell.cpp
+++ MiniShell.cpp
@@ -1,6 +1,6 @@
 /*
 	Name: Elizabeth Brooks
-	Date Modified: April 21, 2016
+	Date Modified: April 28, 2016
 	File: MiniShell
 */
 //Imports
@@ -16,14 +16,14 @@ using namespace std;
 //Function for tokenizing the user input arguments
 vector<string> split(const string &sIn, char del) {
 	//Declare variables for tokenization
-    stringstream subS(sIn);
+    stringstream subS(sIn); //Set string to be tokenized
+    //Prepare substring
     string ss;
     vector<string> tokenizedS;
     //Loop through the string for sub strings
-    while (getline(subS, ss, del))
-    {
+    while (getline(subS, ss, del)) {
         tokenizedS.push_back(ss);
-    }
+    } //End while
     //Return the tokenized string
     return tokenizedS;
 } //End split
@@ -36,49 +36,34 @@ int main(int argc, char* argv[])
 	int resultArgs = 0; //Result of user input function/args
     string tempString; //Temporary string used for determining result to be displayed
     //Loop until the user selects to exit    
-	while(runShell)
-	{
-		//Retrieve user input,
+	while(runShell) { //Retrieve user input while the exit command has not been entered
         getline(cin, inputArgs);
         std::vector<std::string> tokens;     
 		tokens= split(inputArgs,' '); 
-		//then perform the function (arg) entered by the user
-		if(tokens[0].compare("exit") ==0) //1. exit command operation
-        {
-            if(tokens.size() == 2)
-            {
-            	//Return result of operation
-                return std::stoi(tokens[1]);
+		//Perform the function (arg) entered by the user
+		if(tokens[0].compare("exit") == 0) { //1. exit command operation
+            if(tokens.size() == 2) {
+                return std::stoi(tokens[1]); //Return result of operation
             } //End inner if
-            //Result of operation
-            return resultArgs;
-        } else if(tokens[0].compare("mkdir")==0) //2. mkdir command operation
-        {
-            if(tokens.size() >1)
-            {
-                for(int i=1; i<tokens.size();i++)
-                {
+            return resultArgs; //Return result of operation
+        } else if(tokens[0].compare("mkdir") == 0) { //2. mkdir command operation
+            if(tokens.size() >1) {
+                for(int i=1; i<tokens.size(); i++) {
                     tempString.append(tokens.at(i));
                     tempString.append(" ");
                 } //End inner for
-                //Result of operation
-                resultArgs = mkdir(tempString.c_str(), S_IRWXU | S_IRWXG | S_IROTH | S_IXOTH);
+                resultArgs = mkdir(tempString.c_str(), S_IRWXU | S_IRWXG | S_IROTH | S_IXOTH); //Result of operation
             } //End outer for
             //Result of operation
             resultArgs = mkdir("./Default",S_IRWXU | S_IRWXG | S_IROTH | S_IXOTH);
-        } else if(tokens[0].compare("cmp")==0) //4. cmp command operation
-        {
-        	//Result of operation
-            resultArgs = system(std::string("cmp " + tokens[1] + " " + tokens[2]).c_str());
-        } else if(tokens[0].compare("ls")==0) //3. ls command operation
-        {
-        	//Result of operation
-            resultArgs = system("ls");
-        } else 
-        {
-        	
+        } else if(tokens[0].compare("cmp") == 0) //4. cmp command operation
+            resultArgs = system(std::string("cmp " + tokens[1] + " " + tokens[2]).c_str()); //Result of operation
+        } else if(tokens[0].compare("ls") == 0) { //3. ls command operation
+            resultArgs = system("ls"); //Result of operation
+        } else { //Display error message
+            cout << "Incorrect input...";
         }//End else, if
+        cout << resultArgs; //Print result of operation
 	} //End while
-	//Program complete, exit return
-	return 0;
+	return 0; //Program complete, exit return
 } //End main
